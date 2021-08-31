dc7416f902a75908f5998f95e7c8ba9cff193809
Jack <jackrceballos@gmail.com>
2021-08-30 21:11:16 -0400

    Finished with the design of the navbar. I just need to change the font.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
+import { Nav, Navbar, Container, Col } from 'react-bootstrap';
 
 class Navigation extends React.Component {
   constructor(props) {
@@ -13,23 +13,14 @@ class Navigation extends React.Component {
   render() {
 
     return (
-      <Navbar bg="secondary" text='light' expand="md">
+      <Navbar bg="secondary">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
+          <Col xs={12}/>
+          <Nav className="me-auto text-light">
+            <Nav.Link href="#home" className='text-light'>ABOUT</Nav.Link>
+            <Nav.Link href="#portfolio" className='text-light'>PORTFOLIO</Nav.Link>
+            <Nav.Link href="#contact" className='text-light'>CONTACT</Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
     )

b39281554ea3f6d053b73e009513a9070d0d5ab3
Jack Ceballos <jackrceballos@gmail.com>
2021-09-21 22:45:53 -0400

    Getting the routes ready for react-router-dom.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Navigation from './components/Nav/Nav.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navigation/>
-    </div>
+    <Router>
+      <div>
+        <Navigation />
+        {/* <Route exact path='/' component={Main} />
+        <Route exact path='/' component={Portfolio} />
+        <Route exact path='/' component={Contact} /> */}
+      </div>
+    </Router>
   );
 }
 

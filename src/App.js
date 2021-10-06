e1cd8c7c57ac76fb8d7e935ed2910306669ca502
Jack Ceballos <jackrceballos@gmail.com>
2021-10-06 15:15:44 -0400

    Fixing the react-dom-router to set up the page.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import Navigation from './components/Nav/Nav.js';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Main from './components/pages/main/Main';
+import Portfolio from './components/pages/portfolio/Portfolio';
+import Contact from './components/pages/contact/Contact';
 import './App.min.css';
 
 function App() {
@@ -7,9 +10,9 @@ function App() {
     <Router>
       <div>
         <Navigation />
-        {/* <Route exact path='/' component={Main} />
-        <Route exact path='/' component={Portfolio} />
-        <Route exact path='/' component={Contact} /> */}
+        <Route exact path='/' component={Main} />
+        <Route exact path='/portfolio' component={Portfolio} />
+        <Route exact path='/contact' component={Contact} />
       </div>
     </Router>
   );
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,32 +1,22 @@
 import React from 'react';
 import { Nav, Navbar, Container, Col } from 'react-bootstrap';
 
-class Navigation extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      page: 'home'
-    }
-  }
-  
-  
-  render() {
-
-    return (
-      <Container>
-        <Navbar bg="secondary">
-          <Container>
-            <Col />
-            <Nav className="me-auto text-light">
-              <Nav.Link href="#home" className='text-light'>ABOUT</Nav.Link>
-              <Nav.Link href="#portfolio" className='text-light'>PORTFOLIO</Nav.Link>
-              <Nav.Link href="#contact" className='text-light'>CONTACT</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-      </Container>
-    )
-  }
+function Navigation() {
+  return (
+    <Container>
+      <Navbar bg="secondary">
+        <Container>
+          <Col />
+          <Nav className="me-auto text-light">
+            <Nav.Link href="/" className='text-light'>ABOUT</Nav.Link>
+            <Nav.Link href="/portfolio" className='text-light'>PORTFOLIO</Nav.Link>
+            <Nav.Link href="/contact" className='text-light'>CONTACT</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </Container>
+  )
 }
+
 
 export default Navigation;

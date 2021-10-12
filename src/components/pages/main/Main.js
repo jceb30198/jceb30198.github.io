3201f032b83c3888368aa339eab688c323a5acb6
Jack Ceballos <jackrceballos@gmail.com>
2021-10-11 23:39:47 -0400

    Fixing the layout of the page.

--- src/components/pages/main/Main.js
+++ src/components/pages/main/Main.js
@@ -1,17 +1,23 @@
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card, Image } from 'react-bootstrap';
+import portrait from '../../../assets/IMG_1095.jpg';
 
 function Main() {
   return (
     <Container>
       <Row>
         <Col>
-          <h1>Image</h1>
-        </Col>
-        <Col>
-          <Card bg='secondary'>
-            <Card.Title className='text-light text-center'>About Me</Card.Title>
-            <Card.Body>
+          <Card bg='secondary' text='light' className='mb-2'>
+            <Card.Title className='text-center'><h3>A Little About Me</h3></Card.Title>
+          </Card>
 
+          <Card bg='dark' className='mb-2'>
+              <Image rounded width='425px' height='525px' className='mx-auto' src={portrait}/>
+          </Card>
+
+          <Card bg='secondary' text='light'>
+            
+            <Card.Body>
+              <p>Hello World</p>
             </Card.Body>
           </Card>
         </Col>

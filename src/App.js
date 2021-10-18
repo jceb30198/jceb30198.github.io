34d9a6c663bef931978deaafb2394a3a5694be33
Jack Ceballos <jackrceballos@gmail.com>
2021-10-18 18:47:34 -0400

    Still working on the routing problem.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import Navigation from './components/Nav/Nav.js';
 import Footer from './components/Footer/Footer.js';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Main from './components/pages/Main/Main';
 import Portfolio from './components/pages/Portfolio/Portfolio';
 import './App.min.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navigation />
-        <Route exact path='/' component={Main} />
-        <Route exact path='/portfolio' component={Portfolio} />
+    <div>
+      <Navigation />
+      <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route exact path='/' component={Main} />
+          <Route exact path='/portfolio' component={Portfolio} />
+        </Switch>
         <Footer />
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 

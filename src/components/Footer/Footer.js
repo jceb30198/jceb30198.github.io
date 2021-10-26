d2d2255e0c59921daa43f50315b52a811777e9c9
Jack Ceballos <jackrceballos@gmail.com>
2021-10-26 12:42:28 -0400

    Fixing formatting

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import { Row, Col } from 'react-bootstrap';
-import resume from '../../assets/JackCeballosResume.pdf';
+import resume from '../../assets/JackRCeballosResume.pdf';
 
 function Footer() {
   const styles = {
@@ -18,33 +18,33 @@ function Footer() {
 
   return (
     <footer className='text-light bg-secondary p-3' style={styles.root}>
-    <Row>
-      <Col>
-        <h6>Made with React-Boostrap and Boostrap.</h6>
-      </Col>
-      <Col>
-        <h6>Contact Information:</h6>
-        <ul style={styles.list}>
-          <li>jackrceballos@gmail.com</li>
-          <li>(305) 772-1065</li>
-        </ul>
-      </Col>
-      <Col>
-        <h6>Links:</h6>
+      <Row>
+        <Col>
+          <h6>Made with React-Boostrap and Boostrap.</h6>
+        </Col>
+        <Col>
+          <h6>Contact Information:</h6>
+          <ul style={styles.list}>
+            <li>jackrceballos@gmail.com</li>
+            <li>(305) 772-1065</li>
+          </ul>
+        </Col>
+        <Col>
+          <h6>Links:</h6>
           <ul style={styles.list}>
             <li><a href='/' className='text-light' style={styles.link}>Home</a></li>
             <li><a href='#/portfolio' className='text-light' style={styles.link}>Portfolio</a></li>
             <li>
-            <a href='https://www.linkedin.com/in/jackceballosmia/' className='text-light' style={styles.link} target='_blank' rel='noreferrer'>LinkedIn</a>
+              <a href='https://www.linkedin.com/in/jackceballosmia/' className='text-light' style={styles.link} target='_blank' rel='noreferrer'>LinkedIn</a>
             </li>
             <li>
-            <a href='https://github.com/jceb30198' className='text-light' style={styles.link} target='_blank' rel='noreferrer'>GitHub</a>
+              <a href='https://github.com/jceb30198' className='text-light' style={styles.link} target='_blank' rel='noreferrer'>GitHub</a>
             </li>
             <li>
               <a href={resume} className='text-light' style={styles.link} target='_blank' rel='noreferrer'>Resume</a>
-              </li>
+            </li>
           </ul>
-      </Col>
+        </Col>
       </Row>
     </footer>
   )
